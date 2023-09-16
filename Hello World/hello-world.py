28d637364d9bfd3f669e3eecf9a88132fbf22cde
Gupples <85248061+Gupples@users.noreply.github.com>
2023-09-16 14:17:17 -0600

    Added comments to clarify code

--- Hello World/hello-world.py
+++ Hello World/hello-world.py
@@ -1,5 +1,17 @@
+# Import datetime to enable time functionality.
+from datetime import datetime
+
 def main():
+    # Display "Hello World!"
     print("Hello World!")
+    # Ask the user for their name.
+    name = input("What is your name? \n> ")
+    # Note the current date and time from their computer. 
+    time = datetime.now()
+    # Greet the user.
+    print(f"It's nice to meet you, {name}!")
+    # Acknowledge when the user entered their name.
+    print(f"{time.strftime('I met you on %A, %B %d at %I:%M %p.')} ")
     
 
 # Enable testing with pytest.
